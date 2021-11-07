0c282813ad0e74efc045d0a18928020a43a75778
david-beaudette <beaudette.david@gmail.com>
2021-11-07 05:08:53 -0500

    Integrating rp2040 source in project.

--- src/blink.c
+++ src/blink.c
@@ -1,15 +1,11 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+/** STAN THE STANDING ROBOT
+   Program for the base / low-level controller 
+   by David Beaudette   
+**/
 
 #include "pico/stdlib.h"
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -19,5 +15,4 @@ int main() {
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
-#endif
 }
